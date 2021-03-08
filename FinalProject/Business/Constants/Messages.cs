788a0a6833b1f8c5f4fb702c68cf753c0fd60701
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-03-08 13:45:40 +0300

    Update messages class

--- FinalProject/Business/Constants/Messages.cs
+++ FinalProject/Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoryde en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists = "Bu isimde başka bir ürün vardır.";
         public static string CategoryLimitExceded = "Category sayısı aşımı yaşanmıştır yeni ürün eklenemiyor.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Parola hatası.";
+        public static string SuccessfulLogin = "Başarılı giriş";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
+        internal static string UserRegistered = "Kayıt başarılı.";
     }
 }
