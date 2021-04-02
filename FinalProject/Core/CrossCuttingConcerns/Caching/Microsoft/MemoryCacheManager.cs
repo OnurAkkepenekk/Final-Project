4e9e989baa6fd1e49b9b6bcf6946b4645bab81ff
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-04-02 12:14:42 +0300

    Add caching

--- FinalProject/Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
+++ FinalProject/Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -1,41 +1,70 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Core.CrossCuttingConcerns.Caching.Microsoft
 {
     public class MemoryCacheManager : ICacheManager
     {
-        IMemoryCache memoryCache;
+        // Adapter Pattern
+        IMemoryCache _memoryCache;
+        public MemoryCacheManager()
+        {
+            _memoryCache = ServiceTool.ServiceProvider.GetService<IMemoryCache>();
+        }
+
         public void Add(string key, object value, int duration)
         {
-            throw new NotImplementedException();
+            _memoryCache.Set(key, value, TimeSpan.FromMinutes(duration));
         }
 
         public T Get<T>(string key)
         {
-            throw new NotImplementedException();
+            return _memoryCache.Get<T>(key);
         }
 
         public object Get(string key)
         {
-            throw new NotImplementedException();
+            return _memoryCache.Get(key);
         }
 
         public bool IsAdd(string key)
         {
-            throw new NotImplementedException();
+            //data istemiyorum var mı yok mu onu kontrol et
+            return _memoryCache.TryGetValue(key, out _);
         }
 
         public void Remove(string key)
         {
-            throw new NotImplementedException();
+            _memoryCache.Remove(key);
         }
-
+        // Calısma anında bellekten silmeye yarar.
         public void RemoveByPattern(string pattern)
         {
-            throw new NotImplementedException();
+            // Bellekte EntriesCollection bul
+            var cacheEntriesCollectionDefinition = typeof(MemoryCache).GetProperty("EntriesCollection", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            // Definition'ı _memoryCache olanları bul 
+            var cacheEntriesCollection = cacheEntriesCollectionDefinition.GetValue(_memoryCache) as dynamic;
+            List<ICacheEntry> cacheCollectionValues = new List<ICacheEntry>();
+
+            foreach (var cacheItem in cacheEntriesCollection)
+            {
+                ICacheEntry cacheItemValue = cacheItem.GetType().GetProperty("Value").GetValue(cacheItem, null);
+                cacheCollectionValues.Add(cacheItemValue);
+            }
+
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var keysToRemove = cacheCollectionValues.Where(d => regex.IsMatch(d.Key.ToString())).Select(d => d.Key).ToList();
+
+            foreach (var key in keysToRemove)
+            {
+                _memoryCache.Remove(key);
+            }
         }
     }
 }
